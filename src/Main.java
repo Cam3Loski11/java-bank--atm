a3c279482bccaf4abbaba08fbab5bc759531e690
Cam3Loski11 <jfcamelo.2004@gmail.com>
2024-09-19 16:31:31 -0400

    starting: adding simple code to Main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-      System.out.println("Welcome to JavaBank ATM! Version control with Git.");
+      // Variables básicas
+      String accountNumber = "123456789";
+      double balance = 1500.75;
+      int correctPin = 1234;
+
+      // Array de montos de transacciones
+      int[] transactionAmounts = {200, -100, 50};
+
+      // Operaciones con variables
+      balance += transactionAmounts[0]; // Depósito
+
+      // Variables de entrada
+      System.out.println("Ingresa tu pin: ");
+      Scanner scanner = new Scanner(System.in);
+      int userPin = scanner.nextInt();
+
+      if (balance > 0 && userPin == correctPin) { 
+        System.out.println("Acceso Concedido");
+      } else {
+        System.out.println("Acceso Denegado");
+
+        System.exit(0);
+      }
+
+      scanner.close();
+
+      // Uso de operadores
+      balance++; // Incremento
+      String status = (balance < 0) ? "Deuda" : "Crédito";
+      System.out.println("Estado de cuenta: " + status);
   }
 }
 
